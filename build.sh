3663354e1793413db4fd4e769d8ad667f0e7a6d1
Falko Zurell <falko.zurell@ubirch.com>
2016-05-20 21:29:19 +0200

    fixed build variables
    preparation for GO-Server
    approved by @leo

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-TOOLCHAIN=../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake
+TOOLCHAIN=${TOOLCHAIN:-"../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake"}
 SDK_NAME=SDK_2.0_MK82FN256xxx15
+SDK_ROOT=${SDK_ROOT:-"../ubirch-kinetis-sdk"}/$SDK_NAME
 MCU=MK82F25615
 if [ "$1" == "-a" ]
 then BUILDS="Debug Release MinSizeRel RelWithDebInfo"
@@ -12,7 +13,7 @@ for BUILD_TYPE in $BUILDS
 do
   (mkdir -p "build/$MCU/$BUILD_TYPE"; cd "build/$MCU/$BUILD_TYPE"; \
   cmake ../../.. -DCMAKE_TOOLCHAIN_FILE="../../../$TOOLCHAIN" \
-    -DSDK_ROOT="../$SDK_NAME" \
+    -DSDK_ROOT=$SDK_ROOT \
     -DMCU="$MCU" \
     -DCMAKE_BUILD_TYPE="$BUILD_TYPE"; \
   make)
