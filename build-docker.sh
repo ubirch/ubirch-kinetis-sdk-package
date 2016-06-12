fcd6316e0b57e972a910ee8b5893d1fd182217ef
Falko Zurell <falko.zurell@ubirch.com>
2016-06-12 15:06:25 +0200

    added more environment variables to docker build
    pass more environment variables during docker build

--- build-docker.sh
+++ build-docker.sh
@@ -5,9 +5,8 @@ ARM_CONTAINER_VERSION="latest"
 
 
 function init() {
-
-  DEPENDENCY_LABEL=`env | grep GO_DEPENDENCY_LABEL_ | cut -d= -f2 | sed 's/\s//g' | tr -d '\n'`
-
+  # this requires the Material for the pipeline to be named "ARM_BUILD_CONTAINER"
+  DEPENDENCY_LABEL=`env | grep GO_DEPENDENCY_LABEL_ARM_BUILD_CONTAINER | cut -d= -f2 | sed 's/\s//g' | tr -d '\n'`
 
   if [ -z ${DEPENDENCY_LABEL} ]; then
     ARM_CONTAINER_VERSION="latest"
@@ -19,7 +18,7 @@ function init() {
 }
 
 function build_software() {
-  docker run --user `id -u`:`id -g` -e "HOME=/build" --workdir=/build/ubirch-kinetis-sdk-package --volume=${PWD}/..:/build --env=${SDK_ROOT}  --env=${TOOLCHAIN} --entrypoint=/bin/bash ubirch/arm-build:${ARM_CONTAINER_VERSION} ./build.sh
+  docker run --user `id -u`:`id -g` -e "HOME=/build" --workdir=/build/ubirch-kinetis-sdk-package --volume=${PWD}/..:/build --env=${SDK_ROOT}  --env=${TOOLCHAIN} --env=${MCU} --entrypoint=/bin/bash ubirch/arm-build:${ARM_CONTAINER_VERSION} ./build.sh
   if [ $? -ne 0 ]; then
       echo "Docker build failed"
       exit 1
--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
-#! /bin/bash 
+#!/bin/bash -x
 TOOLCHAIN=${TOOLCHAIN:-"../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake"}
-SDK_NAME=SDK_2.0_MK82FN256xxx15
+SDK_NAME=${SDK_NAME:-"SDK_2.0_MK82FN256xxx15"}
 SDK_ROOT=${SDK_ROOT:-"../ubirch-kinetis-sdk"}/$SDK_NAME
-MCU=MK82F25615
+MCU=${MCU:-"MK82F25615"}
 if [ "$1" == "-a" ]
-then BUILDS="Debug Release MinSizeRel RelWithDebInfo"
+  then BUILDS="Debug Release MinSizeRel RelWithDebInfo"
 else BUILDS=MinSizeRel
 fi
+
 # do an out-of-source build for all configurations
 [ -d "build/$MCU" ] && rm -r "build/$MCU"
 for BUILD_TYPE in $BUILDS
