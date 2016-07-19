8c4d12695f6a9c7361269b44d745950a2cdd03c7
Matthias L. Jugel <leo@thinkberg.com>
2016-07-19 10:09:34 +0200

    reduce SDMMC middleware to SD only

--- cmake/KinetisMiddleware.cmake
+++ cmake/KinetisMiddleware.cmake
@@ -28,30 +28,33 @@ macro(AddKinetisMiddlewareFATFS)
     message(STATUS "SDK has FATFS support, adding required sources to FATFS_MIDDLEWARE_SRCS/INC")
 
     set(FATFS_MIDDLEWARE_SRCS
-      "${FATFS_MIDDLEWARE}/fsl_mmc_diskfsl_mmc_disk.c"
+      #      "${FATFS_MIDDLEWARE}/src/fsl_mmc_disk/fsl_mmc_disk.c"
       #      "${FATFS_MIDDLEWARE}/fsl_option/freertos_syscall.c"
       #      "${FATFS_MIDDLEWARE}/fsl_option/ucosii_syscall.c"
       #      "${FATFS_MIDDLEWARE}/fsl_option/ucosiii_syscall.c"
-      "${FATFS_MIDDLEWARE}/fsl_ram_disk/fsl_ram_disk.c"
-      "${FATFS_MIDDLEWARE}/fsl_sd_disk/fsl_sd_disk.c"
-      "${FATFS_MIDDLEWARE}/fsl_sdspi_disk/fsl_sdspi_disk.c"
-      "${FATFS_MIDDLEWARE}/fsl_usb_disk/fsl_usb_disk_bm.c"
+#      "${FATFS_MIDDLEWARE}/src/fsl_ram_disk/fsl_ram_disk.c"
+      "${FATFS_MIDDLEWARE}/src/fsl_sd_disk/fsl_sd_disk.c"
+      #      "${FATFS_MIDDLEWARE}/src/fsl_sdspi_disk/fsl_sdspi_disk.c"
+      #      "${FATFS_MIDDLEWARE}/src/fsl_usb_disk/fsl_usb_disk_bm.c"
       #      "${FATFS_MIDDLEWARE}/fsl_usb_disk/fsl_usb_disk_freertos.c"
-      "${FATFS_MIDDLEWARE}/option/cc932.c"
-      "${FATFS_MIDDLEWARE}/option/cc936.c"
-      "${FATFS_MIDDLEWARE}/option/cc949.c"
-      "${FATFS_MIDDLEWARE}/option/cc950.c"
-      "${FATFS_MIDDLEWARE}/option/ccsbcs.c"
-      "${FATFS_MIDDLEWARE}/option/syscall.c"
-      "${FATFS_MIDDLEWARE}/option/unicode.c"
+      #      "${FATFS_MIDDLEWARE}/src/option/cc932.c"
+      #      "${FATFS_MIDDLEWARE}/src/option/cc936.c"
+      #      "${FATFS_MIDDLEWARE}/src/option/cc949.c"
+      #      "${FATFS_MIDDLEWARE}/src/option/cc950.c"
+      #      "${FATFS_MIDDLEWARE}/src/option/ccsbcs.c"
+      "${FATFS_MIDDLEWARE}/src/option/syscall.c"
+      "${FATFS_MIDDLEWARE}/src/option/unicode.c"
+      "${FATFS_MIDDLEWARE}/src/diskio.c"
+      "${FATFS_MIDDLEWARE}/src/ff.c"
       )
 
     set(FATFS_MIDDLEWARE_INC
-      "${FATFS_MIDDLEWARE}/fsl_mmc_disk"
-      "${FATFS_MIDDLEWARE}/fsl_ram_disk"
-      "${FATFS_MIDDLEWARE}/fsl_sd_disk"
-      "${FATFS_MIDDLEWARE}/fsl_sdspi_disk"
-      "${FATFS_MIDDLEWARE}/fsl_usb_disk"
+      "${FATFS_MIDDLEWARE}/src/fsl_mmc_disk"
+      "${FATFS_MIDDLEWARE}/src/fsl_ram_disk"
+      "${FATFS_MIDDLEWARE}/src/fsl_sd_disk"
+      "${FATFS_MIDDLEWARE}/src/fsl_sdspi_disk"
+      "${FATFS_MIDDLEWARE}/src/fsl_usb_disk"
+      "${FATFS_MIDDLEWARE}/src"
       )
 
     set(FATFS_MIDDLEWARE_INCL "set(FATFS_MIDDLEWARE_INC ${FATFS_MIDDLEWARE_INC} CACHE PATH \"\")")
@@ -85,10 +88,10 @@ endmacro()
 
 # SDMMC middleware
 macro(AddKinetisMiddlewareSDMMC)
-  find_file(HAS_SDHC NAMES ${KSDK_ROOT}/devices/${MCU}/drivers/fsl_sdhc.h)
+  find_file(HAS_SDHC NAMES fsl_sdhc.h PATHS ${KSDK_ROOT}/devices/${MCU}/drivers)
   file(GLOB SDMMC_MIDDLEWARE ${KSDK_ROOT}/middleware/sdmmc*)
   list(LENGTH SDMMC_MIDDLEWARE HAS_SDMMC_MIDDLEWARE)
-  if (HAS_SDHA AND HAS_SDMMC_MIDDLEWARE EQUAL 1)
+  if (HAS_SDHC AND HAS_SDMMC_MIDDLEWARE EQUAL 1)
     message(STATUS "SDK has SD/MMC support, adding library libsdmmc.a")
     set(TARGET_SDMMC sdmmc)
 
